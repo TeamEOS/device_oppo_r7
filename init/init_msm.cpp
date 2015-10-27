23e458adee33de917fad4c223b9b28dddcaa2183
Christopher N. Hesse <raymanfx@gmail.com>
2015-10-27 19:23:08 +0100

    r7: Convert init library to cpp
    Change-Id: Ibc1165ae9818fe59883ff52b5f48ba05f5e79c62

--- init/init_msm.cpp
+++ init/init_msm.cpp
@@ -31,6 +31,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <private/android_filesystem_config.h>
--- init/init_msm8916.cpp
+++ init/init_msm8916.cpp
@@ -28,6 +28,7 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
