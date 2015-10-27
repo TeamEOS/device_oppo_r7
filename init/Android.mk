53345964a453bb1717d73f0d12e28d9d74424464
Christopher N. Hesse <raymanfx@gmail.com>
2015-10-27 18:52:30 +0100

    r7: Cleanup init library
    Change-Id: I6639b3b510d4e696bb078bbff500a1ec70a6c513

--- init/Android.mk
+++ init/Android.mk
@@ -1,20 +1,12 @@
 ifeq ($(TARGET_INIT_VENDOR_LIB),libinit_msm_r7)
 
 LOCAL_PATH := $(call my-dir)
-LIBINIT_MSM_PATH := $(call my-dir)
-
-LIBINIT_USE_MSM_DEFAULT := $(shell if [ ! -f $(LIBINIT_MSM_PATH)/init_$(TARGET_BOARD_PLATFORM).c ]; then echo true; fi)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_msm.c
-ifeq ($(LIBINIT_USE_MSM_DEFAULT),true)
-  LOCAL_SRC_FILES += init_msmdefault.c
-else
-  LOCAL_SRC_FILES += init_$(TARGET_BOARD_PLATFORM).c
-endif
+LOCAL_SRC_FILES := init_msm.c init_msm8916.c
 LOCAL_MODULE := libinit_msm_r7
 include $(BUILD_STATIC_LIBRARY)
 
